d1e37b152d959b258ac4ad16e6f1c28b20a8f57a
Matteo Collina <hello@matteocollina.com>
2015-10-27 12:10:36 +0000

    Added HAPI-based server.

---
+++ server.js
@@ -0,0 +1,44 @@
+'use strict'
+
+var Chairo = require('chairo')
+var Hapi = require('hapi')
+var Joi = require('joi')
+
+var server = new Hapi.Server()
+server.connection({ port: 3000 })
+
+server.register({ register: Chairo }, function (err) {
+
+  server.seneca.use('devices')
+
+  server.route({
+    method: 'GET',
+    path: '/devices',
+    handler: function (request, reply) {
+      return reply.act({ role: 'devices', cmd: 'list' })
+    }
+  })
+
+  server.route({
+    method: 'POST',
+    path: '/devices',
+    config: {
+      validate: {
+        payload: {
+          name: Joi.string().min(3).max(10)
+        }
+      }
+    },
+    handler: function (request, reply) {
+      return reply.act({
+        role: 'devices',
+        cmd: 'save',
+        name: request.payload.name
+      })
+    }
+  })
+})
+
+server.start(function () {
+  console.log('Server running at:', server.info.uri)
+})
